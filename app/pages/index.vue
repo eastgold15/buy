dd88b11fe73ac85e63f56e10e108013a9123ed16
点正 <1960825664@qq.com>
2025-02-26 18:36:45 +0800

    git init

---
+++ app/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'home',
+})
+</script>
+
+<template>
+  <div class="page-index">
+    <TheLogos />
+    <TheExamples />
+  </div>
+</template>
+
+<style>
+.page-index {
+  padding-top: 60px;
+  text-align: center;
+}
+</style>
